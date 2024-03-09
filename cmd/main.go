21ee0e605db4b6fb245ec202ba2d82be50aeb1d7
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-03-09 12:25:05 +0300

    postgres connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,23 @@
-package cmd
+package main
 
-import "DatabaseHandler/pkg/config"
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
 
 func main() {
-	var app config.AppConfig
+	//var app config.AppConfig
 
+	var connStr string
+	connStr = "postgres://postgres:root@localhost:5432/goptest?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 }
